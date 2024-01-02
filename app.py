d37c06032a94e07e57059fa1e64c46f7250a35f0
Reven8e <y.s.y.t147@gmail.com>
2024-01-02 16:42:43 +0200

    added sync functions for new users

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, join_room, leave_room
+from flask_socketio import SocketIO, join_room, emit
+
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = os.environ['FLASK_SECRET']
 socketio = SocketIO(app)
 
 
-@app.route('/watch_video/<code>')
-def watch_video(code):
+@app.route('/watch_video/<session_code>')
+def watch_video(session_code):
     video_path = '/static/video1/output.m3u8'
-    return render_template('watch.html', video_url=video_path, session_code=code)
+    return render_template('watch.html', video_url=video_path, session_code=session_code)
+
 
 @socketio.on('join')
 def on_join(data):
-    room = data['session_code']
-    join_room(room)
-
-@socketio.on('leave')
-def on_leave(data):
-    room = data['session_code']
-    leave_room(room)
+    room_code = data['session_code']
+    join_room(room_code)
+    emit('new_user_joined', {'session_code': room_code}, room=room_code)
 
 @socketio.on('sync_command')
 def handle_sync_command(data):
-    action = data.get('action')
-    currentTime = data.get('currentTime')
-    session_code = data.get('session_code')
+    session_code = data['session_code']
+    emit('sync_action', data, room=session_code, include_self=False)
 
-    socketio.emit('sync_action', {
-        'action': action,
-        'currentTime': currentTime
-    }, room=session_code, include_self=False)
+@socketio.on('report_current_time')
+def handle_report_current_time(data):
+    session_code = data['session_code']
+    emit('update_time', {'currentTime': data['currentTime']}, room=session_code)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, port=5003)
