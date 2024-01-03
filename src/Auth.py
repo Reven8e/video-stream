b8e541654158778ca625f4f6aef643ff1e8b4659
Reven8e <y.s.y.t147@gmail.com>
2024-01-03 11:31:50 +0200

    authentication

--- src/Auth.py
+++ src/Auth.py
@@ -6,7 +6,7 @@ from src import login_required
 auth = Blueprint('auth', __name__)
 
 
-@auth.route("/sign-in", methods=["GET", "POST"])
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form.get('username')
@@ -14,13 +14,50 @@ def login():
 
         db = DBMS()
         statement, user = db.login_user(username, password)
+        db.close_connection()
 
         if statement is True:
             session['logged_in'] = True
             session['user'] = user
-            return redirect(url_for('views.home')), flash("Signed in in successfully!", category='success')
+            return redirect(url_for('watch.html')), flash("Signed in in successfully!", category='success')
         
         else:
-            flash(user, category='error')
+            flash(user, category='danger')
 
     return render_template("login.html")
+
+@auth.route("/signup", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(username) < 3:
+            flash("First name must be greater than 3 characters.", category='danger')
+
+        elif password1 != password2:
+            flash("Passwords dont match.", category='error')
+        
+        elif len(password1) < 7:
+            flash("Password must be at least 7 characters.", category='danger')
+
+        else:
+            db = DBMS()
+            statement, data = db.register_user(username, password1)
+            db.close_connection()
+
+            if statement is True:
+                session['logged_in'] = True
+                session['user'] = data
+                return redirect(url_for('watch.html')), flash("Account created successfully!", category='success')
+            else:
+                flash(data, category='danger')
+
+    return render_template("signup.html")
+
+@auth.route('/sign-out')
+@login_required
+def sign_out():
+    session.clear()
+    return redirect(url_for('auth.logins')), flash('You have signed out.', 'success')
