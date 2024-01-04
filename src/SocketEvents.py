e0947e7e7f3eaf2d0d483cd2bf13eda93cbda345
Reven8e <y.s.y.t147@gmail.com>
2024-01-04 13:11:00 +0200

    namespacing socketio

--- src/SocketEvents.py
+++ src/SocketEvents.py
@@ -1,21 +1,23 @@
 from flask import Blueprint
-from flask_socketio import SocketIO, join_room, leave_room
+from flask_socketio import Namespace, join_room, emit
+
+from src import socketio
 
 socket_events = Blueprint('socket_events', __name__)
-socketio = SocketIO()
 
-@socketio.on('join', namespace='/socket_events')
-def on_join(data):
-    room_code = data['session_code']
-    join_room(room_code)
-    socketio.emit('new_user_joined', {'session_code': room_code}, room=room_code)
 
-@socketio.on('sync_command', namespace='/socket_events')
-def handle_sync_command(data):
-    session_code = data['session_code']
-    socketio.emit('sync_action', data, room=session_code, include_self=False)
+class StreamManager(Namespace):
+    def on_join(self, data):
+        room_code = data['session_code']
+        join_room(room_code)
+        emit('new_user_joined', {'session_code': room_code}, room=room_code)
 
-@socketio.on('report_current_time', namespace='/socket_events')
-def handle_report_current_time(data):
-    session_code = data['session_code']
-    socketio.emit('update_time', {'currentTime': data['currentTime']}, room=session_code)
+    def on_sync_command(self, data):
+        session_code = data['session_code']
+        emit('sync_action', data, room=session_code, include_self=False)
+
+    def on_report_current_time(self, data):
+        session_code = data['session_code']
+        emit('update_time', {'currentTime': data['currentTime']}, room=session_code)
+
+socketio.on_namespace(StreamManager('/StreamManager'))
