b5bebe7cdfe9c947e4e1078f13098cfc985b2a57
Reven8e <y.s.y.t147@gmail.com>
2024-01-04 13:12:03 +0200

    added error handler to code generation

--- src/CodeManage.py
+++ src/CodeManage.py
@@ -18,12 +18,13 @@ class CodeManage:
         chars = string.ascii_letters + string.digits
         return ''.join(random.choice(chars) for _ in range(self.__SIZE))
     
-    def generate_access_code(self, user_id: str, movie_id: int, expiration_date: datetime) -> str:
+    def generate_access_code(self, user_id: str, movie_id: int, expiration_date: datetime) -> str or None:
         random_code = self.__generate_random_code()
 
-        self.db.insert_access_code(random_code, movie_id, user_id, expiration_date)
-
-        return random_code
+        if self.db.insert_access_code(random_code, movie_id, user_id, expiration_date):
+            return random_code
+        else:
+            return
     
     def check_access_code(self, access_code: str) -> bool:
         return self.db.check_access_code(access_code)
